94c6930ef87eb52837faa473f77b3a89253dbaf7
neevlokchandani <73300169+neevlokchandani@users.noreply.github.com>
2021-09-19 10:32:20 +0530

    Firebase

---
+++ config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+import "firebase/auth"
+import "firebase/database"
+import "firebase/app"
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCb90RCi_vtvQNillUip_9sqmwtpDlxc5U",
+  authDomain: "spectagram-1762c.firebaseapp.com",
+  projectId: "spectagram-1762c",
+  storageBucket: "spectagram-1762c.appspot.com",
+  messagingSenderId: "505919300036",
+  appId: "1:505919300036:web:c20e9bbeab05a7a6230491"
+};
